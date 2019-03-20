6abf91e2632f74eb01302c9ca8976650fa6fd506
rockstarcreativestudio <support@rockstarcreativestudio.com>
2019-03-20 12:42:27 -0400

    Updates

---
+++ precache-manifest.7bace856708043cb70f8ebf5c17f6f36.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "73649db39fa5d86660bf",
+    "url": "/React-Weather-App/static/js/runtime~main.b230b676.js"
+  },
+  {
+    "revision": "1c07a0a30469ea410dba",
+    "url": "/React-Weather-App/static/js/main.8a1758ec.chunk.js"
+  },
+  {
+    "revision": "397f3442efc0d26e04c9",
+    "url": "/React-Weather-App/static/js/2.464cd558.chunk.js"
+  },
+  {
+    "revision": "1c07a0a30469ea410dba",
+    "url": "/React-Weather-App/static/css/main.4fb48f05.chunk.css"
+  },
+  {
+    "revision": "698c8feff820eac53acec3bad9155789",
+    "url": "/React-Weather-App/index.html"
+  }
+];
